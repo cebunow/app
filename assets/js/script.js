ff8e6563ab610242a883b4b8f636dbc128539434
openwolfco <support@openwolf.com>
2020-05-30 01:50:54 +0800

    first update

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("cebu_total").innerHTML = myObj.cebu_total;
+    document.getElementById("cebu_deaths").innerHTML = myObj.cebu_deaths;
+    document.getElementById("nationwide").innerHTML = myObj.nationwide;
+    document.getElementById("cebu_city").innerHTML = myObj.cebu_city;
+    document.getElementById("mandaue").innerHTML = myObj.mandaue;
+    document.getElementById("lapulapu").innerHTML = myObj.lapulapu;
+    document.getElementById("time_updated").innerHTML = myObj.time_updated;
+
+  }
+};
+
+
+
+xmlhttp.open("GET", "../../data/covid.json", true);
+xmlhttp.send();
